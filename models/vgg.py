bf3a637ab6a721f51f2404741b5bc31db549b4f2
ignitemorale <125619233+ignitemorale@users.noreply.github.com>
2023-03-30 20:11:29 +0900

    Update vgg.py

--- models/vgg.py
+++ models/vgg.py
@@ -40,7 +40,7 @@ class VGG(nn.Module):
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
         return nn.Sequential(*layers)
     
-    def beforeclass():
+    def data():
         global bclf
         bs=bclf.size()[1]
         fp1=open('train_lable.txt','w')
