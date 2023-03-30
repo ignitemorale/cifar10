a1067c6f5dd8f46196cfe46654dc68ca7947edf9
ignitemorale <125619233+ignitemorale@users.noreply.github.com>
2023-03-30 14:32:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ if args.resume:
     # Load checkpoint.
     print('==> Resuming from checkpoint..')
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    checkpoint = torch.load(f'./checkpoint/{netname}.pth')
     net.load_state_dict(checkpoint['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
